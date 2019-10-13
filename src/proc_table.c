12a0726c7c881a10a116eb0720a3095b104a847c
David Gillis <dgillis91@gmail.com>
2019-10-13 18:30:37 -0500

    refactor to use pcb_table struct

--- src/proc_table.c
+++ src/proc_table.c
@@ -5,9 +5,11 @@ static const uint32_t ONE = 1;
 static uint32_t taken_pid_bitmap = 0;
 static uint8_t processes_allocated_count = 0;
 
+static pcb_table_t* process_table = NULL;
+
 
 static unsigned int next_available_pid() {
-    uint32_t available_pid_bitmap = ~taken_pid_bitmap;
+    uint32_t available_pid_bitmap = ~process_table->process_allocation_bitmap;
     unsigned int first_available_pid = __builtin_ffs(available_pid_bitmap);
     return first_available_pid;
 }
@@ -15,14 +17,14 @@ static unsigned int next_available_pid() {
 
 static void setbit(unsigned int index) {
     uint32_t bitpos = ONE << index;
-    taken_pid_bitmap |= taken_pid_bitmap;
+    process_table->process_allocation_bitmap |= bitpos;
 }
 
 
 static void unsetbit(unsigned int index) {
     uint32_t bitpos = ONE << index;
     bitpos = ~bitpos;
-    taken_pid_bitmap &= bitpos;
+    process_table->process_allocation_bitmap &= bitpos;
 }
 
 
@@ -30,14 +32,14 @@ static void unsetbit(unsigned int index) {
  * `PROCESS_BUFFER_LENGTH` PCBs in it.
  */
 int is_process_buffer_full() {
-    return processes_allocated_count == PROCESS_BUFFER_LENGTH;
+    return process_table->count_processes_allocated == PROCESS_BUFFER_LENGTH;
 }
 
 
 /* Find the next available PID, and allocate it. 
  */
 int allocate_next_pid() {
-    if (processes_allocated_count >= PROCESS_BUFFER_LENGTH) {
+    if (process_table->count_processes_allocated >= PROCESS_BUFFER_LENGTH) {
         return -1;
     }
     unsigned int next_pid = next_available_pid();
