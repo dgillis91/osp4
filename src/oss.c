0e8c1190a0c7e650f0755199debbacd6cdf88e4e
David Gillis <dgillis91@gmail.com>
2019-10-14 20:19:14 -0500

    test clock

--- src/oss.c
+++ src/oss.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <stdio.h>
+#include <limits.h>
 
 #include "../include/proc_table.h"
 #include "../include/pclock.h"
@@ -15,8 +16,17 @@ static int clock_shid;
 int main(int argc, char* argv[]) {
     clock_shid = init_clock(CLOCK_KEY);
     if (clock_shid == -1) {
-        destruct_clock()
+        perror("Failed to allocate clock");
+        destruct_clock(clock_shid);
+        return EXIT_FAILURE;
     }
 
+    unsigned long i;
+    for (i = 0; i < NANO_SEC_IN_SEC * 20; i += 100000000) {
+        tick_clock(100000000);
+        printf("%u.%u: %lu\n", get_seconds(), get_nano(), get_total_tick());
+    }
+
+    destruct_clock(clock_shid);
     return EXIT_SUCCESS;
 }
